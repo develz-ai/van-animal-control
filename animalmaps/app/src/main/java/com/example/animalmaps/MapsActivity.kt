131b62a9e08ba7f05a2d6c771d592474a762301e
derek <derekliu26@gmail.com>
2022-02-20 21:16:12 -0800

    map and how to take animal done
    navbar still working

---
+++ animalmaps/app/src/main/java/com/example/animalmaps/MapsActivity.kt
@@ -0,0 +1,48 @@
+package com.example.animalmaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.animalmaps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val VAC = LatLng(49.273689179312015, -123.0819799923655)
+        mMap.addMarker(MarkerOptions().position(VAC).title("Marker in Vancouver Animal Control"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(VAC, 15f))
+    }
+}
---
+++ howtotakecareofpet/app/src/main/java/com/oddinstitute/howtotakecareofpet/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.oddinstitute.howtotakecareofpet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.howtotakecareofpet.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myTitle.text = "kotlin"
+    }
+}
