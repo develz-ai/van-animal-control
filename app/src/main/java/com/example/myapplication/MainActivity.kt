0da07920f856a964a8c4d2b40c41345575d0edda
Michael <michaelpsu604@gmail.com>
2022-03-22 12:53:45 -0700

    Navigation fixed
    Except some pages crash for some reason

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,27 +10,24 @@ import com.example.myapplication.databinding.ActivityMainBinding
 import android.widget.ImageView
 import android.widget.LinearLayout
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-
-class MainActivity : AppCompatActivity() {
-
-//    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_toolbar)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-
-//        val view = binding.root
-//        setContentView(view)
 
 
-//        binding.homeFrame1.setOnClickListener(this)
-//        binding.homeFrame2.setOnClickListener(this)
-//        binding.homeFrame3.setOnClickListener(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.homeFrame1.setOnClickListener(this)
+        binding.homeFrame2.setOnClickListener(this)
+        binding.homeFrame3.setOnClickListener(this)
 
 //        val button1 = findViewById<Button>(R.id.adopt_button)
 //        button1.setOnClickListener{
@@ -48,15 +45,37 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(intent)
 //        }
 
+//        setContentView(R.layout.activity_toolbar)
+//        setSupportActionBar(findViewById(R.id.toolbar))
+
+//        var toolbar = ToolBarActivity()
+//        toolbar.show(supportFragmentManager, "CustomDialog")
+
     }
-//
-//    override fun onClick(view: View?) {
-//        val intent = Intent(this, InventoryActivity::class.java)
-//
-//        startActivity(intent)
-//
-//
-//    }
+
+    override fun onClick(view: View?) {
+        //val intent = Intent(this, InventoryActivity::class.java)
+
+        //startActivity(intent)
+
+        val button1 = binding.homeFrame1
+        button1.setOnClickListener{
+            val intent = Intent(this, InventoryActivity::class.java)
+            startActivity(intent)
+        }
+        val button2 = binding.homeFrame2
+        button2.setOnClickListener{
+            val intent = Intent(this, PrepActivity::class.java)
+            startActivity(intent)
+        }
+        val button3 = binding.homeFrame3
+        button3.setOnClickListener{
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
 
 
 
