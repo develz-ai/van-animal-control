52a4933735ca6e117cea4761edd23ddf08f09dcb
Michael <michaelpsu604@gmail.com>
2022-03-22 15:34:27 -0700

    Pages no longer crash
    yeehaw

--- app/src/main/java/com/example/myapplication/InventoryActivity.kt
+++ app/src/main/java/com/example/myapplication/InventoryActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityInventoryBinding
 
 class InventoryActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityInventoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory)
+
+        binding = ActivityInventoryBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+//        binding.animal1.setOnClickListener(this)
+
+        binding.animal1.setOnClickListener{
+            val intent = Intent(this, AnimalDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,8 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import android.view.View
-import android.widget.Button
 import com.example.myapplication.databinding.ActivityMainBinding
-import android.widget.ImageView
-import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,46 +13,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val view = binding.root
         setContentView(view)
 
-
-        binding.homeFrame1.setOnClickListener(this)
-        binding.homeFrame2.setOnClickListener(this)
-        binding.homeFrame3.setOnClickListener(this)
-
-//        val button1 = findViewById<Button>(R.id.adopt_button)
-//        button1.setOnClickListener{
-//            val intent = Intent(this, InventoryActivity::class.java)
-//            startActivity(intent)
-//        }
-//        val button2 = findViewById<Button>(R.id.learn_pet_button)
-//        button2.setOnClickListener{
-//            val intent = Intent(this, PrepActivity::class.java)
-//            startActivity(intent)
-//        }
-//        val button3 = findViewById<Button>(R.id.find_shelter_button)
-//        button3.setOnClickListener{
-//            val intent = Intent(this, MapActivity::class.java)
-//            startActivity(intent)
-//        }
-
-//        setContentView(R.layout.activity_toolbar)
-//        setSupportActionBar(findViewById(R.id.toolbar))
-
-//        var toolbar = ToolBarActivity()
-//        toolbar.show(supportFragmentManager, "CustomDialog")
-
-    }
-
-    override fun onClick(view: View?) {
-        //val intent = Intent(this, InventoryActivity::class.java)
-
-        //startActivity(intent)
+//        binding.homeFrame1.setOnClickListener(this)
+//        binding.homeFrame2.setOnClickListener(this)
+//        binding.homeFrame3.setOnClickListener(this)
 
         val button1 = binding.homeFrame1
         button1.setOnClickListener{
@@ -74,14 +38,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             startActivity(intent)
         }
 
+//        setContentView(R.layout.activity_toolbar)
+//        setSupportActionBar(findViewById(R.id.toolbar))
+
+//        var toolbar = ToolBarActivity()
+//        toolbar.show(supportFragmentManager, "CustomDialog")
 
     }
 
+    override fun onClick(view: View?) {
+        //val intent = Intent(this, InventoryActivity::class.java)
+
+        //startActivity(intent)
+
+//        val button1 = binding.homeFrame1
+//        button1.setOnClickListener{
+//            val intent = Intent(this, InventoryActivity::class.java)
+//            startActivity(intent)
+//        }
+//        val button2 = binding.homeFrame2
+//        button2.setOnClickListener{
+//            val intent = Intent(this, PrepActivity::class.java)
+//            startActivity(intent)
+//        }
+//        val button3 = binding.homeFrame3
+//        button3.setOnClickListener{
+//            val intent = Intent(this, MapActivity::class.java)
+//            startActivity(intent)
+//        }
 
 
-
-
-
-
+    }
 
 }
