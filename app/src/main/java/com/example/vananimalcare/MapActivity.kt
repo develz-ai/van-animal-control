4f397d4c61beceb29bcb7437a47b4dfd72a28936
Michael <michaelpsu604@gmail.com>
2022-03-23 00:48:40 -0700

    Map + markers are fully functional
    Just need to add every animal shelter in vancouver now

--- app/src/main/java/com/example/vananimalcare/MapActivity.kt
+++ app/src/main/java/com/example/vananimalcare/MapActivity.kt
@@ -2,9 +2,12 @@ package com.example.vananimalcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -17,9 +20,24 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(gMap: GoogleMap) {
-        myMap = gMap
+        myMap = gMap!!
+        val latLng = LatLng(49.2537719,-123.1454474)
+
+
+        val markerOptions : MarkerOptions =
+            MarkerOptions().position(latLng).title("QEP")
+
+        myMap.addMarker(markerOptions)
+
+        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11.0f))
+
+        myMap.setOnMarkerClickListener {
+            marker -> marker.showInfoWindow()
+            true
+        }
     }
 }
